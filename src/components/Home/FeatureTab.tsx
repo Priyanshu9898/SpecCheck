87ed87d5934eb8691b953a9c49cddce29ef5dd4d
Priyanshu9898 <priyanshu.mce20@sot.pdpu.ac.in>
2024-04-19 16:33:18 +0530

    Features section is created

---
+++ src/components/Home/FeatureTab.tsx
@@ -0,0 +1,78 @@
+// components/FeatureTab.tsx
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+interface FeatureTabProps {
+  title: string;
+  description1: string;
+  description2: string;
+  imageUrl: string;
+  header1: string;
+  header2: string;
+  linkUrl: string;
+}
+
+const FeatureTab: React.FC<FeatureTabProps> = ({
+  title,
+  description1,
+  description2,
+  header1,
+  header2,
+  imageUrl,
+  linkUrl,
+}) => {
+  return (
+    <div className="flex flex-col md:flex-row justify-between items-start gap-x-10 my-8">
+      <div className="w-full md:w-[42%] p-2 md:p-6 text-left">
+        <h3 className="text-3xl md:text-5xl font-bold mb-4 leading-[-1]">
+          {title}
+        </h3>
+        <div className="mt-10">
+          <div className="flex flex-row items-center justify-start">
+            <Image
+              src="/Images/arrow.svg"
+              alt="=>"
+              width={25}
+              height={25}
+              className="mr-4"
+            />
+            <h3 className="text-2xl font-semibold">{header1}</h3>
+          </div>
+          <p className="text-md mt-4 leading-relaxed mb-4">{description1}</p>
+          <hr className="my-4 h-1" />
+          <div className="flex flex-row items-center justify-start">
+            <Image
+              src="/Images/arrow.svg"
+              alt="=>"
+              width={25}
+              height={25}
+              className="mr-4"
+            />
+            <h3 className="text-2xl font-semibold">{header2}</h3>
+          </div>
+          <p className="text-md mt-4  leading-relaxed mb-4">{description2}</p>
+        </div>
+        <Link
+          href={linkUrl}
+          className="flex items-center mt-4 text-blue-600 hover:text-blue-800 transition-colors"
+        >
+          <span className="mr-2">Learn More</span>
+          <Image src="/icons/arrow-right.svg" alt="" width={24} height={24} />
+        </Link>
+      </div>
+      <div className="w-full md:w-1/2">
+        <Image
+          src={imageUrl}
+          alt="Feature image"
+          layout="responsive"
+          width={1980}
+          height={1200}
+          priority
+        />
+      </div>
+    </div>
+  );
+};
+
+export default FeatureTab;
--- src/components/Home/Features.tsx
+++ src/components/Home/Features.tsx
@@ -2,6 +2,7 @@
 
 import { motion } from "framer-motion";
 import React, { ReactNode, useState, useTransition } from "react";
+import FeatureTab from "./FeatureTab";
 interface Tab {
   id: number;
   title: string;
@@ -12,11 +13,63 @@ const Features = () => {
     {
       id: 0,
       title: "All-In-One Dashboard",
-      content: <p>Content for Dashboard</p>,
+      content: (
+        <FeatureTab
+          title="Everything opticians need, in one place."
+          header1="Order Management"
+          header2="Lab Communication"
+          description1="View all glasses orders and live statuses with your lab partners in one place. Send your lab partners orders with our easy-to-use order creation tool."
+          description2="SpecCheck's full suite of products are optimized across all devices. Access our dashboard from anywhere with an internet connection, with no downloads or installs required."
+          imageUrl="/Images/feature-image.png"
+          linkUrl="/live-chat"
+        />
+      ),
     },
-    { id: 1, title: "Lab Pay", content: <p>Content for Lab Pay</p> },
-    { id: 2, title: "Order Entry", content: <p>Content for Order Entry</p> },
-    { id: 3, title: "Lab Live Chat", content: <p>Content for Live Chat</p> },
+    {
+      id: 1,
+      title: "Lab Pay",
+      content: (
+        <FeatureTab
+          title="Automatic billing."
+          header1="Online Billing Portal"
+          header2="Fully Integrated Solution"
+          description1="Facilitate easy and secure payments between eye care professionals and their lab partners. Digitally access all historical lab statements, individual job invoices, and more."
+          description2="SpecCheck's billing product is fully integrated with the lab's LMS, saving your finance team valuable time by automatically posting and reconciling accounts receivable."
+          imageUrl="/Images/labPay.png"
+          linkUrl="/live-chat"
+        />
+      ),
+    },
+    {
+      id: 2,
+      title: "Order Entry",
+      content: (
+        <FeatureTab
+          title="Fast and accurate order entry."
+          header1="Easy-to-use Platform"
+          header2="Lowest Redo Rate"
+          description1="Our platform is thoughtfully crafted to cater to both seasoned and novice opticians. Every feature ensures a smooth and efficient user experience."
+          description2="We take pride in our industry-leading data accuracy! Our platform has the lowest redo rate of any order entry platform."
+          imageUrl="/Images/orderEntry.png"
+          linkUrl="/live-chat"
+        />
+      ),
+    },
+    {
+      id: 3,
+      title: "Lab Live Chat",
+      content: (
+        <FeatureTab
+          title="Immediate lab live chat support."
+          header1="Reduces Call Volume"
+          header2="Seamlessly Integrated"
+          description1="Experience real-time support with our Live Chat! Opticians can now swiftly connect with lab customer service agents, eliminating tedious hold times on calls. Resolve issues faster and enjoy chat history that's stored forever."
+          description2="Lab Live Chat is built into every page of SpecCheck's platform, making it easy for opticians to get fast support from anywhere."
+          imageUrl="/Images/chat.png"
+          linkUrl="/live-chat"
+        />
+      ),
+    },
   ];
   const [activeTab, setActiveTab] = useState(tabs[0].id);
   const [isPending, startTransition] = useTransition();
@@ -28,7 +81,7 @@ const Features = () => {
   };
 
   return (
-    <div className="container mx-auto px-4 md:px-16 py-20 lg:px-40 overflow-hidden">
+    <div className="container mx-auto px-4 md:px-16 py-20 lg:px-32 overflow-hidden">
       <div className="flex flex-col items-center justify-center text-center">
         <motion.h1
           initial={{ opacity: 0, y: 50 }}
@@ -42,7 +95,7 @@ const Features = () => {
           </span>{" "}
         </motion.h1>
 
-        <div className="bg-white border-2 w-full md:w-3/4 mt-10 border-[#727272] shadow-[3px_3px_0px_0_rgba(114,114,114,1)]  rounded-[30px] md:rounded-full px-2 py-1 mb-[80px]">
+        <div className="bg-white border-2 w-full md:w-3/4 mt-8 border-[#727272] shadow-[3px_3px_0px_0_rgba(114,114,114,1)]  rounded-[30px] md:rounded-full px-2 py-1 mb-[40px]">
           <div
             className={`flex flex-wrap ${
               tabs.length > 3 ? "flex-col" : "flex-row"
@@ -64,9 +117,9 @@ const Features = () => {
             ))}
           </div>
         </div>
-        <div className="mt-4">
+        <div className="mt-0">
           {isPending ? (
-            <p>Loading...</p>
+            <></>
           ) : (
             tabs.find((tab) => tab.id === activeTab)?.content
           )}
